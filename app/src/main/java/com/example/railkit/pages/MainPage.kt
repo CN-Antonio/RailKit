c16b23141ab0a08fb4109429faddc818fec32585
Antonio <dongyixiao2001@163.com>
2024-11-09 15:27:54 +0800

    added three cards for main page

--- app/src/main/java/com/example/railkit/pages/MainPage.kt
+++ app/src/main/java/com/example/railkit/pages/MainPage.kt
@@ -1,19 +1,64 @@
 package com.example.railkit.pages
 
 import androidx.compose.runtime.Composable
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.TextField
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.unit.*
+
 
 @Composable
 fun MainPage(string: String) {
-    TextField(
-        value = string,
-        onValueChange = { /* TODO: handle text changes */ },
+    Box(
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(top = 100.dp)
-    )
+            .fillMaxSize()
+            .background(Color(0xFFF5F5F5))  // Light gray background
+    ) {
+        Column (
+            modifier = Modifier
+                .fillMaxWidth()
+                .statusBarsPadding()
+                .padding(horizontal = 8.dp)
+        ){
+            Card( // 查询车票
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color(0xFFFFFFFF)),
+                shape = RoundedCornerShape(12.dp)
+            ) {
+                Text(
+                    text = "Train Search",
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+            Card( // Services
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 10.dp),
+                shape = RoundedCornerShape(12.dp)
+            ) {
+                // Functions: n×5 layout
+                // max 3×5 with page scroll
+                Text(
+                    text = "Functions",
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+            Card( // News
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 10.dp),
+                shape = RoundedCornerShape(12.dp)
+            ) {
+                Text(
+                    text = "News",
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+    }
 }
+//定位，显示最近车站，点击跳转车站相关选项
